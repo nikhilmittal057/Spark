f2fb6169a8c3d832dc60b94d455a5b658a5d2189
nikhilmittal057 <nikhilmittal.exe@gmail.com>
2020-04-18 02:10:04 +0545

    modify-code

--- Scala/src/main/scala/com/scala/core/SocieteGeneral.scala
+++ Scala/src/main/scala/com/scala/core/SocieteGeneral.scala
@@ -10,13 +10,13 @@ object SocieteGeneral extends App {
   val graphDF = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("/home/nikhil/Desktop/Datasets/edges.csv").cache()
   val src_from_df =  graphDF.groupBy("from").agg(count("*").alias("connecting_count")).withColumnRenamed("from","vertices").cache()
   val dest_to_df = graphDF.groupBy("to").agg(count("*").alias("connecting_count")).withColumnRenamed("to", "vertices").cache()
-  val degrees = src_from_df.union(dest_to_df).groupBy("vertices").agg(sum("connecting_count").alias("degree")).cache()
+  val Nodes_degrees = src_from_df.union(dest_to_df).groupBy("vertices").agg(sum("connecting_count").alias("degree")).cache()
 
   val e = graphDF.count()
-  val n = degrees.count()
+  val n = Nodes_degrees.count()
   val AverageDegree =  2*(e/n)
 
-  val finalDF=  degrees.where($"degree" >= AverageDegree).cache()
+  val finalDF=  Nodes_degrees.where($"degree" >= AverageDegree).cache()
   val sorted_rdd = finalDF.sort($"degree".desc, $"vertices".asc).rdd
 // val sorted_rdd =  finalDF.sort(desc("degree"), asc("vertices")).rdd
   val pairedRDD = sorted_rdd.map(tuple => (tuple.get(0).asInstanceOf[Int], tuple.get(1).asInstanceOf[Long])).collect()
