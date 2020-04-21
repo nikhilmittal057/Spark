7422b4e1fd38dc2d8ee46eec996369aa3696d4bd
nikhilmittal057 <nikhilmittal.exe@gmail.com>
2020-04-22 04:29:51 +0545

    modify code

--- Scala/src/main/scala/com/scala/core/sapientQ1.scala
+++ Scala/src/main/scala/com/scala/core/sapientQ1.scala
@@ -10,23 +10,27 @@ object sapientQ1 {
     // val fname = "/home/nikhil/Desktop/Datasets/sapientQ1.csv"
     // val fSource = Source.fromFile(fname)
 
-    val list = Source.fromFile("/home/nikhil/Desktop/Datasets/sapientQ1.csv").getLines().drop(1)
+   /* val list = Source.fromFile("/home/nikhil/Desktop/Datasets/sapientQ1.csv").getLines().drop(1)
       //.filter(line => !line.isEmpty)
       .map(line =>{
       val value = line.split(",")
         (value(0), value(1))
     })
       .toList
-//   val a = list.toSet
+
+   val unique = list.toSet
+    unique.foreach(println)
  //   val a = list.distinct
-    
-
-    //val a = Set(list: _*)
- val test = list.toSet
-
-test.foreach(println)
-
-
+ // val a = Set(list: _*)*/
+  Source.fromFile("/home/nikhil/Desktop/Datasets/sapientQ1.csv").getLines()
+      .foldLeft(Set.empty[String])
+      {
+        case (seen, str) =>
+        val substr = str.take(str.lastIndexOf(","))
+        if (!seen(substr))
+          println(str)  //send to StdOut
+        seen + substr
+      }
 
   }
 
