7e61290efbe2df5cd70d6e4b2c8a80965dfb7dcb
nikhilmittal057 <nikhilmittal.exe@gmail.com>
2020-04-23 17:25:08 +0545

    modify code

--- Scala/src/main/scala/com/scala/core/Friendsbyage.scala
+++ Scala/src/main/scala/com/scala/core/Friendsbyage.scala
@@ -16,8 +16,8 @@ object Friendsbyage extends App {
 
   val totalsByAge = tuple_rdd.mapValues(x => (x,1)).reduceByKey((x,y) =>(x._1+y._1,x._2+y._2)).cache()
 
-//  val averagesByAge = totalsByAge.mapValues(x=> x._1/x._2).cache()
- val averagesByAge = totalsByAge.map(x=> (x._1, x._2._1/x._2._2)).cache()
+  val averagesByAge = totalsByAge.mapValues(x=> x._1/x._2).cache()
+// val averagesByAge = totalsByAge.map(x=> (x._1, x._2._1/x._2._2)).cache()
   val results = averagesByAge.collect()
   println("Age,"+ "Average Of Friends")
   for (data <- results.sorted)
