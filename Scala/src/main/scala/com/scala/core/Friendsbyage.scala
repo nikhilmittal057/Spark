0aa8c03a7bfbf7f9c76e25c73d2ef08efefc9780
nikhilmittal057 <nikhilmittal.exe@gmail.com>
2020-04-23 18:01:34 +0545

    modify code

--- Scala/src/main/scala/com/scala/core/Friendsbyage.scala
+++ Scala/src/main/scala/com/scala/core/Friendsbyage.scala
@@ -1,7 +1,7 @@
 package com.scala.core
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark._
-import org.apache.spark.SparkContext._
-object Friendsbyage extends App {
+object Friendsbyage  {
 
   def parseLine(line: String) = {
     val fields = line.split(",")
@@ -9,20 +9,27 @@ object Friendsbyage extends App {
     val numFriends = fields(3).toInt
     (age, numFriends)
   }
-  val sc = new SparkContext("local[*]", "RatingCounter")
-  val linesRDD = sc.textFile("/home/nikhil/Desktop/Datasets/friends").cache()
-  val tuple_rdd = linesRDD.map(parseLine)
 
 
-  val totalsByAge = tuple_rdd.mapValues(x => (x,1)).reduceByKey((x,y) =>(x._1+y._1,x._2+y._2)).cache()
+  def main(args: Array[String]): Unit = {
 
-  val averagesByAge = totalsByAge.mapValues(x=> x._1/x._2).cache()
-// val averagesByAge = totalsByAge.map(x=> (x._1, x._2._1/x._2._2)).cache()
-  val results = averagesByAge.collect()
-  println("Age,"+ "Average Of Friends")
-  for (data <- results.sorted)
-    println(data._1+",  "+data._2)
-  //results.sorted.foreach(println)
+     Logger.getLogger("org").setLevel(Level.ERROR)
+    val sc = new SparkContext("local[*]", "friendsByAge")
+    val linesRDD = sc.textFile("/home/nikhil/Desktop/Datasets/friends").cache()
+    val tuple_rdd = linesRDD.map(parseLine)
+
+
+    val totalsByAge = tuple_rdd.mapValues(x => (x, 1)).reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2)).cache()
+
+    val averagesByAge = totalsByAge.mapValues(x => x._1 / x._2).cache()
+    // val averagesByAge = totalsByAge.map(x=> (x._1, x._2._1/x._2._2)).cache()
+    val results = averagesByAge.collect()
+    println("Age," + "Average Of Friends")
+    for (data <- results.sorted)
+      println(data._1 + ",  " + data._2)
+    //results.sorted.foreach(println)
+  }
+
 }
 
 
